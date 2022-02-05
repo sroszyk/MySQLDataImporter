902f0f1d98009810c43afe226dc311e00170c2d8
Szymon Roszyk <roszyk.szymon@gmail.com>
2022-02-05 19:58:36 +0100

    Set .

--- MySQLDataImport/Program.cs
+++ MySQLDataImport/Program.cs
@@ -8,11 +8,16 @@ IConfiguration config = new ConfigurationBuilder()
     .AddEnvironmentVariables()
     .Build();
 
+var currentCulture = System.Globalization.CultureInfo.InstalledUICulture;
+var numberFormat = (System.Globalization.NumberFormatInfo)currentCulture.NumberFormat.Clone();
+numberFormat.NumberDecimalSeparator = ".";
+
 var connectionString = config.GetConnectionString("MyDatabase");
 
 var dbContext = new mydatabaseContext(connectionString);
 
 List<Gwtable> data = new List<Gwtable>();
+
 
 using (var reader = new StreamReader("DaneDoBazy.txt"))
 {
@@ -26,17 +31,17 @@ using (var reader = new StreamReader("DaneDoBazy.txt"))
             PozRej = int.Parse(dataTable[2].Trim()),
             NazwiskoImieDoOplatyNaleznosci = dataTable[3].Trim(),
             NazwaWsiLubUlicyNumerDomu = dataTable[4].Trim(),
-            PowMeliorowana = double.Parse(dataTable[5].Trim()),
+            PowMeliorowana = double.Parse(dataTable[5].Trim(), numberFormat),
             UdzialUlamkowyWpozRej = dataTable[6].Trim(),
-            NaleznoscOgolemDoZaplaty = double.Parse(dataTable[7].Trim()),
-            ZalegloscZlatUbieglych = double.Parse(dataTable[8].Trim()),
-            NaliczoneOdsetkiZaZwloke = double.Parse(dataTable[9].Trim()),
-            KosztyUpomnieniaDoZaplaty = double.Parse(dataTable[10].Trim()),
-            Umorzenie = double.Parse(dataTable[11].Trim()),
-            DodatkoweObciazenieNalezn = double.Parse(dataTable[12].Trim()),
-            WysokoscWplatyOgolem = double.Parse(dataTable[13].Trim()),
-            WysokoscWplatyWtZalegl = double.Parse(dataTable[14].Trim()),
-            Saldo = double.Parse(dataTable[15].Trim()),
+            NaleznoscOgolemDoZaplaty = double.Parse(dataTable[7].Trim(), numberFormat),
+            ZalegloscZlatUbieglych = double.Parse(dataTable[8].Trim(), numberFormat),
+            NaliczoneOdsetkiZaZwloke = double.Parse(dataTable[9].Trim(), numberFormat),
+            KosztyUpomnieniaDoZaplaty = double.Parse(dataTable[10].Trim(), numberFormat),
+            Umorzenie = double.Parse(dataTable[11].Trim(), numberFormat),
+            DodatkoweObciazenieNalezn = double.Parse(dataTable[12].Trim(), numberFormat),
+            WysokoscWplatyOgolem = double.Parse(dataTable[13].Trim(), numberFormat),
+            WysokoscWplatyWtZalegl = double.Parse(dataTable[14].Trim(), numberFormat),
+            Saldo = double.Parse(dataTable[15].Trim(), numberFormat),
         });;
     }
 }
